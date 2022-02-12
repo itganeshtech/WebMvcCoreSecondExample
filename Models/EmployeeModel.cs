1f8fb28ea32db00f4d2e69e313aa9c629524b152
itganeshtech <it.ganesh.tech@gmail.com>
2022-02-13 03:43:09 +0530

    Add project files.

---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvcCoreSecondExample.Models
+{
+    public class EmployeeModel
+    {
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public string Salary { get; set; }
+        public string Website { get; set; }
+    }
+}
